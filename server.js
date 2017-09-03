4fc5e623d9e46131d323f3cb3ce4db772ceda98f
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2017-09-03 14:37:58 +0545

    Add @currentUsers command

--- server.js
+++ server.js
@@ -9,6 +9,30 @@ app.use(express.static(__dirname + '/public'));
 
 var clientInfo = {};
 
+// Sends current users to provided socket
+function sendCurrentUsers(socket) {
+    var info = clientInfo[socket.id];
+    var users = [];
+
+    if (typeof info === 'undefined') {
+        return users;
+    }
+
+    Object.keys(clientInfo).forEach((socketId) => {
+        var userInfo = clientInfo[socketId];
+
+        if (info.room === userInfo.room) {
+            users.push(userInfo.name);
+        }
+    });
+    
+    socket.emit('message', {
+        name: 'System',
+        text: 'Current users: ' + users.join(', '),
+        timestamp: moment().valueOf()
+    })
+}
+
 io.on('connection', (socket) => {
     console.log('User connected via socket.io');
 
@@ -38,9 +62,13 @@ io.on('connection', (socket) => {
     socket.on('message', (message) => {
         console.log('Message received: ' + message.text);
 
-        message.timestamp = moment().valueOf();
-        // io.emit everyone including sender
-        io.to(clientInfo[socket.id].room).emit('message', message); // everyone except sender
+        if (message.text === '@currentUsers') {
+            sendCurrentUsers(socket);
+        } else {
+            message.timestamp = moment().valueOf();
+            // io.emit everyone including sender
+            io.to(clientInfo[socket.id].room).emit('message', message); // everyone except sender
+        }
     });
 
     // timestamp property - JS timestamp (milliseconds)
